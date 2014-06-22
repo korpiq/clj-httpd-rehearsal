e8ce91ae96da745be34f78619ebc512e96a27a0d
Kalle Hallivuori <kato@iki.fi>
2014-06-22 22:37:48 +0300

    allow any JSON in test response with added json library dependency for test profile.

--- test/httpd/core_test.clj
+++ test/httpd/core_test.clj
@@ -1,11 +1,12 @@
 (ns httpd.core-test
   (:require [clojure.test :refer :all]
-            [httpd.core :refer :all]))
+            [httpd.core :refer :all]
+            [clojure.data.json :as json]))
 
-(deftest app-ok-test
-  (testing "app will return valid JSON as a response to a request."
-    (let [expected_result
-          {:status 200,
-           :headers {"Content-Type" "application/json; charset=utf-8"},
-           :body "{\"hello\":\"World!\"}"}]
-      (is (= expected_result (app {}))))))
+(deftest app-response-is-valid-test
+  (let [actual_result (app {})]
+    (testing "app will return valid headers for JSON as a response to a request."
+      (is (= 200 (:status actual_result)))
+      (is (= {"Content-Type" "application/json; charset=utf-8"} (:headers actual_result))))
+    (testing "app will return valid JSON in body of a response to a request."
+      (is (map? (json/read-str (:body actual_result)))))))
